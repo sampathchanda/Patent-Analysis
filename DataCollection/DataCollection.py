27910ef538070b42b2b216ae20e3f361c34379f9
Sampath Chanda <sunny.sam369@gmail.com>
2016-11-10 02:35:28 -0500

    Basic Data Collection
    1. DataCollection.py to contain all functions for data collection.
    Currently has basic function to consume .json and dump list of titles
    into .pkl file
    2. Added description regarding Data Collection in midterm report
    notebook
    3. Updating 2016.pkl file using latest script

---
+++ DataCollection/DataCollection.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 10 02:09:08 2016
+
+@author: sam
+"""
+
+import ijson
+import pickle
+
+def dataCollection(year):
+    filename = "%s.json" % year
+    pkl_file = "%s.pkl" % year
+    pkl_handle = open(pkl_file, 'wb')
+    with open(filename, 'rb') as in_file:
+        keyData = []
+        for prefix, event, value in ijson.parse(in_file):
+            record = ""
+            if prefix.endswith('.webURI') and (event == 'string'):
+                record = value
+                if record != u' ':
+                    keyData.append(record)
+          
+        # Dump List of titles into a Pickle file
+        pickle.dump(keyData, pkl_handle)
+        pkl_handle.close()
+        
+# Example
+# dataCollection(2016)
+
+def loadPickle(year):
+    pkl_file = "%s.pkl" % year
+    lol = pickle.load(open(pkl_file, "rb"))
