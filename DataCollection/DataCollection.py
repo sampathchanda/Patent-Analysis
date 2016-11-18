9320184f562879d7dae0f8b48a01c9570c43f244
Sampath Chanda <sunny.sam369@gmail.com>
2016-11-17 19:13:28 -0500

    Formatted report for Data collection from USPTO data
    Formatted the data collection part to put things in order and neat

--- DataCollection/DataCollection.py
+++ DataCollection/DataCollection.py
@@ -9,27 +9,37 @@ Created on Thu Nov 10 02:09:08 2016
 import ijson
 import pickle
 import time
+import os
 
-def dataCollection(year):
-    filename = "%s.json" % year
-    pkl_file = "%s.pkl" % year
+def dataCollection(filename):
+    filepath, file_ext = os.path.splitext(filename)
+    pkl_file = filepath + ".pkl"
     pkl_handle = open(pkl_file, 'wb')
+
     with open(filename, 'rb') as in_file:
         keyData = []
+
         for prefix, event, value in ijson.parse(in_file):
+            #print prefix, event, value
             record = ""
-            if prefix.endswith('.webURI') and (event == 'string'):
+            #if prefix.endswith('.webURI') and (event == 'string'):
+            if prefix.endswith('.inventionTitle.content.item') and (event == 'string'):
                 record = value
                 if record != u' ':
                     keyData.append(record)
-          
+
         # Dump List of titles into a Pickle file
         pickle.dump(keyData, pkl_handle)
         pkl_handle.close()
- 
+
+def loadPickle(filename):
+    lol = pickle.load(open(filename, "rb"))
+    return lol
         
 # Example
-dataCollection(2016)
+#dataCollection('2016.json')
+#lol = loadPickle('2016.pkl')
+#print lol
 
 #for yr in [1995]:
 #    start_time = time.time()
@@ -38,6 +48,4 @@ dataCollection(2016)
 
 #    print "Time taken: %s" % (time.time() - start_time)
 
-def loadPickle(year):
-    pkl_file = "%s.pkl" % year
-    lol = pickle.load(open(pkl_file, "rb"))
+
